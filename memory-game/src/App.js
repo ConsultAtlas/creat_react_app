e15fc5bc045c17c41da4b1facb2a578e721a11ab
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2017-12-05 19:57:40 -0500

    began adding game logic

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -2,8 +2,45 @@ import React, { Component } from 'react';
 import './App.css';
 import MemoryCard from './MemoryCard.js';
 
+generateDeck() {
+  var symbols = ["∆", "ß", "£", "§", "•", "$", "+", "ø"];
+  var deck = [];
+
+  for (var i=0; i<16; i++) {
+    allCards.push({
+        isFlipped: false,
+        symbol: symbols[i%8]
+    });
+  }
+    shuffle(deck);
+
+    return deck;
+  
+}
+
+/**
+ * pulled from https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+ */
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+  }
+
 
 class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      deck: [],
+      pickedCards: []
+    };
+  }
+  
   render() {
     return (
         <div className="App">
